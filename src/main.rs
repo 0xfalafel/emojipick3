f8668e990129fc5e9f3aec86e2160d3accf7f435
Lasne Olivier <lasneolivier@gmail.com>
2025-04-22 17:08:20 +0200

    moved EmojiButton to a seperate file

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,9 @@
 use gtk::prelude::*;
-use relm4::factory::{FactoryVecDeque, Position};
-use relm4::factory::positions::GridPosition;
 use relm4::prelude::*;
+use relm4::factory::FactoryVecDeque;
 
-struct EmojiButton {
-    pub symbol: String,
-    pub name: String,
-}
-
-#[relm4::factory]
-impl FactoryComponent for EmojiButton {
-    type Init = (String, String);
-    type Input = ();
-    type Output = ();
-    type CommandOutput = ();
-    type ParentWidget = gtk::Grid;
-
-    view! {
-        gtk::Button {
-            set_label: &self.symbol,
-            set_tooltip: &self.name,
-            connect_clicked[symbol = self.symbol.clone()] => move |_| {
-                println!("You clicked {}", symbol);
-            }
-        }
-    }
-
-    fn init_model(init: Self::Init, _index: &DynamicIndex, _sender: FactorySender<Self>) -> Self {
-        let (symbol, name) = init;
-        Self { symbol, name }
-    }
-}
-
-impl Position<GridPosition, DynamicIndex> for EmojiButton {
-    fn position(&self, index: &DynamicIndex) -> GridPosition {
-        let index = index.current_index();
-        let x = index / 10;
-        let y = index % 10;
-        GridPosition {
-            column: y as i32,
-            row: x as i32,
-            width: 1,
-            height: 1,
-        }
-    }
-}
+mod emojibutton;
+use emojibutton::EmojiButton;
 
 struct App {
     emojis: FactoryVecDeque<EmojiButton>,
